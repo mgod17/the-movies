8e4cbb736d788c026f22e4ab028cb14900ce964f
mgod17 <marianogodoyg@gmail.com>
2023-06-22 21:36:24 -0300

    Se loguea el usuario

--- api/index.js
+++ api/index.js
@@ -1,16 +1,28 @@
-require("./db");
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const port = 5000;
-const usersRouter = require("./routes/users");
 
 // Configurar el middleware para analizar JSON en las solicitudes
 app.use(express.json());
 
+// Configurar el middleware de sesión
+app.use(
+  session({
+    secret: "mysecretkey",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 // Configurar las rutas
+const usersRouter = require("./routes/users");
 app.use("/api/users", usersRouter);
 
 // Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto ${port}`);
 });
+
+// Importar la configuración de la base de datos
+require("./db");
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 // Ruta de registro de usuario
 router.post("/register", (req, res) => {
@@ -13,5 +14,32 @@ router.post("/register", (req, res) => {
       res.status(500).send({ message: "Error creating user" });
     });
 });
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log("Request body:", req.body);
+  // Buscar el usuario en la base de datos por su nombre de usuario
+  User.findOne({ where: { username } })
+    .then((user) => {
+      if (!user) {
+        res.status(401).send({ message: "Credenciales inválidas" });
+        return;
+      }
 
+      // Comparar la contraseña ingresada con la contraseña almacenada en la base de datos
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          // Establecer los datos de sesión en la cookie
+          req.session.username = username;
+          req.session.loggedIn = true;
+          res.status(200).send({ message: "Inicio de sesión exitoso" });
+        } else {
+          res.status(401).send({ message: "Credenciales inválidas" });
+        }
+      });
+    })
+    .catch((error) => {
+      console.log("Error al autenticar al usuario:", error);
+      res.status(500).send({ message: "Error en el servidor" });
+    });
+});
 module.exports = router;
